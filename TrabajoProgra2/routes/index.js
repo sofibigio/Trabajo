d3d15dab248920c5d7047b1c4ad38726b261f348
sofibigio <sbigio@udesa.edu.ar>
2025-04-09 17:08:28 -0300

    Realizado 1 a 5 del punto 1

--- TrabajoProgra2/routes/index.js
+++ TrabajoProgra2/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+const indexController = require ('../controllers/indexController');
+
 module.exports = router;
