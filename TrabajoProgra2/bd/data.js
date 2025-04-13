a31bb99689ca36a39424493d2ebd08c474161d8a
byudachkin <byudachkin@udesa.edu.ar>
2025-04-13 18:15:42 -0300

    terminado css. comentarios y detalle de producto

--- TrabajoProgra2/bd/data.js
+++ TrabajoProgra2/bd/data.js
@@ -10,69 +10,69 @@ productos: [{
     imagen: "/images/products/foto1.jpg",
     descripcion: "Rimel waterproof 24hs",
     nombre: "RIMEL",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Lucia Fernandez", texto:"Muy bueno!", imgaen:"/images/fotoperfil.webp"}, ]
 }, {
     id: 2,
     imagen: "/images/products/foto2.webp",
     descripcion: "Bronzer en polvo sin parabenos",
     nombre: "BRONZER",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Jazmin Perez", texto:"Recomiendo!", imgaen:"/images/fotoperfil.webp"}, ]
 },
 {
     id: 3,
     imagen: "/images/products/foto3.webp",
     descripcion: "Blush nars tono 04",
     nombre: "BLUSH",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Pepe Hernandez", texto:"Malisimo!", imgaen:"/images/fotoperfil.webp"}, ]
 },
 {
     id: 4,
     imagen: "/images/products/foto4.webp",
     descripcion: "Gloss marca Dior tono 002",
     nombre: "GLOSS DIOR",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Sophie Yudachkin", texto:"Muy rapido el envio, y el producto de alta calidad", imgaen:"/images/fotoperfil.webp"}, ]
 },
 {
     id: 5,
     imagen: "/images/products/foto5.avif",
     descripcion: "Paleta de marrones para ojos",
     nombre: "PALETA",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Natalia Geier", texto:"no me gusto", imgaen:"/images/fotoperfil.webp"}, ]
 },
 {
     id: 6,
     imagen: "/images/products/foto6.jpg",
     descripcion: "Base tono medio, marca MAC",
     nombre: "BASE",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Marcelo Yudachkin", texto:"muy buena calidad! productos exportados de USA", imgaen:"/images/fotoperfil.webp"}, ]
 },
 {
     id: 7,
     imagen: "/images/products/foto7.jpg",
     descripcion: "Gel fijador de cejas 24hs, waterproof",
     nombre: "GEL DE CEJAS",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Paula Lucci", texto:"me encanto el tono", imgaen:"/images/fotoperfil.webp"}, ]
 },
 {
     id: 8,
 imagen: "/images/products/foto8.webp",
 descripcion: "Tinta roja tono 04",
 nombre: "BENETINT",
-comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+comentarios: [{nombre: "Lara Bigio", texto:"me gusto, pero muy pequeño el envace", imgaen:"/images/fotoperfil.webp"}, ]
 },
 {
     id: 9,
     imagen: "/images/products/foto9.webp",
     descripcion: "Bronzing drops de Drunk Elephant",
     nombre: "BRONZING DROPS",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Agustina Argibay", texto:"estafa!", imgaen:"/images/fotoperfil.webp"}, ]
 },
 {
     id: 10,
     imagen: "/images/products/foto10.webp",
     descripcion: "Corrector cobertura media tono light",
     nombre: "CORRECTOR",
-    comentarios: [{nombre: "mm", texto:"Cc", imgaen:"mm"}, ]
+    comentarios: [{nombre: "Uma Bravi", texto:"excelente calidad", imgaen:"/images/fotoperfil.webp"}, ]
 },
 
 ]
