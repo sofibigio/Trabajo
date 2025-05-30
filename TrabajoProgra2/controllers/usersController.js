ec8b5a25ca925454ac53faf15952859f890857c4
ambaralianak <aambar@udesa.edu.ar>
2025-05-29 21:13:57 -0300

    Realizado parte del registro

--- TrabajoProgra2/controllers/usersController.js
+++ TrabajoProgra2/controllers/usersController.js
@@ -1,4 +1,9 @@
-db = require('../db/data')
+db = require('../db/data');
+const bcrypt = require('bcryptjs');
+const db = require('../database/models');
+const { DATE } = require('sequelize');
+const Users = db.Users;
+
 
 
 const usersController = {
@@ -12,6 +17,59 @@ const usersController = {
     register: function (req, res) {
         res.render('register')
     },
+    procesarRegister: function (req, res) {
+        let info = req.body;
+        let error = {}; 
+        if (info.nameUsuario== ""){
+            error.message="Nombre de usuario obligatorio";
+            res.locals.errors=error;
+            return res.render('register')
+        } else if (info.password==""){ 
+            error.message="Contraseña obligatoria";
+            res.locals.errors=error;
+            return res.render('register') 
+        
+        }
+        else if (info.email==""){
+             error.message="Email obligatorio";
+            res.locals.errors=error;
+            return res.render('register') 
+        } else if (info.password.length > 3){
+             error.message="Debe contar con mas de 3 caracteres";
+            res.locals.errors=error;
+            return res.render('register') 
+        } else{
+            let foto = req.file.filename
+            let nuevoUsuario = {
+                nombre_usuario: info.nameUsuario,
+                contrasenia: bcrypt.hashSync(info.password, 12),
+                email: info.email,
+                fecha: info.fecha,
+                dni: info.dni,
+                foto_perfil: foto, //preguntar foto
+                created_at: new Date().toISOString, //preguntar como cargar fecha
+                updated_at: info.updated_at //preguntar como cargar 
+            }
+            let repetido = {
+                where: [{email: req.body.email}]
+            }
+            Users.findOne(repetido)
+            .then(function(resultado){
+                if (!resultado){
+                    Users.create(nuevoUsuario)
+                    .then(function(){
+                        res.redirect("/Users/login")
+                    }).catch(error)
+                    
+                }else {
+                     error.message="El usuario ya se encuentra registrado";
+                     res.locals.errors=error;
+                     return res.render('register') 
+                }
+            })
 
+            
+        }
+    } 
 }; 
 module.exports = usersController;
